64606ddb9162a37adf49b15c23a6d38c287e6860
Ahmed Mounsif <ahmounsi@e1r6p1.1337.ma>
2026-01-15 22:45:21 +0100

    a working minimal optimisation

--- push_swap.c
+++ push_swap.c
@@ -12,7 +12,7 @@ int	main(int argc, char **argv)
 
 	stack_a = get_list(argv + 1);
 	stack_b = NULL;
-	if(stack_a)
+	if (stack_a)
 	{
 		stacks.a = &stack_a;
 		stacks.b = &stack_b;
@@ -36,17 +36,35 @@ int	main(int argc, char **argv)
 void	init_stacks(t_stacks stacks)
 {
 	size_t	stack_size;
+	int		new_big;
+	short	first_two;
 
+	first_two = 2;
 	stack_size = ft_lstsize(*(stacks.a));
 	if (stack_size > 3)
 	{
-		stack_size -= 3;
-		while (stack_size--)
+		new_big = INT_MIN;
+		while (stack_size > 3 && first_two--)
+		{
 			p(stacks.b, stacks.a, "b");
+			stack_size--;
+			if (*(int *)(*stacks.b)->content > new_big)
+				new_big = *(int *)((*stacks.b)->content);
+		}
+		while (stack_size > 3)
+		{
+			p(stacks.b, stacks.a, "b");
+			stack_size--;
+			if (*(int *)(*stacks.b)->content > *(int *)((*stacks.b)->next->content))
+				new_big = *(int *)((*stacks.b)->next->content);
+			else if(*(int *)(*stacks.b)->content < new_big / 2)
+				r(stacks.b, "b");
+			// else if(*(int *)(*stacks.b)->content < *(int *)((*stacks.b)->next->content))
+			// 	s(stacks.b, "b");
+		}
 	}
 	hard_sort(stacks.a);
 }
-
 
 static void	examin_status(t_list **stack)
 {
