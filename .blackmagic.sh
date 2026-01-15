ff716b83ef97496c83c200a720fa8a2ec0ca1a5b
Ahmed Mounsif <ahmounsi@e2r13p4.1337.ma>
2026-01-15 14:53:48 +0100

    added checker result checker

--- .blackmagic.sh
+++ .blackmagic.sh
@@ -1,13 +1,15 @@
 TEST=0
 RES=0;
+CHECKER=0;
 MAX=5500;
 COUNT=0
 
 while true;
 do
-	TEST=$(shuf -i  1-500 -n 500)
+	TEST=$(shuf -i  1-500 -n 500 | tr "\n" " ")
 	RES=$(./push_swap $TEST | wc -l)
-	echo $RES at $COUNT
+	CHECKER=$(./push_swap $TEST | ./checker_linux $TEST)
+	echo $RES at $COUNT is $CHECKER
 	((COUNT++))
 	if [ $RES -gt $MAX ]; then
 		echo "Failed!"
