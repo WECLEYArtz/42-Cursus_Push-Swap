857f7756d0310389a6ee01447e1ec9a5e90c2ad2
Ahmed Mounsif <ahmounsi@e2r8p1.1337.ma>
2026-01-19 14:08:24 +0100

    added vogsphere script

---
+++ .vogsphere_cleanup.sh
@@ -0,0 +1,25 @@
+git branch -D vogsphere
+git checkout -b vogsphere
+
+rm -rf *test*
+rm -rf *Test*
+rm -rf a.out*
+
+rm -rf *.a
+rm -rf *.md
+rm -rf *.o
+rm -rf *.txt
+rm -rf .*.txt
+rm -rf *.txt
+
+rm -rf .blackmagic.sh
+rm -rf .gdb.gdb
+rm -rf .gitignore
+rm -rf .gitmodules
+rm -rf .todo.txt
+rm -rf .vogsphere_cleanup.sh
+rm -rf .whitemagic.sh
+
+git add .
+git commit -m "auto delete"
+git push -f 42 vogsphere:main
