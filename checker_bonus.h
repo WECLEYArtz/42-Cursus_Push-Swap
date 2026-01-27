3a0f9c1a278606c41d1db01577787674e1fa2daf
Ahmed Mounsif <ahmounsi@e1r8p6.1337.ma>
2026-01-27 17:50:08 +0100

    fixed headers, no header protection, unused lib

--- checker_bonus.h
+++ checker_bonus.h
@@ -10,9 +10,14 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef CHECKER_BONUS_H
+# define CHECKER_BONUS_H
+
 #include "push_swap.h"
 
 // Bonus helpers
 short	is_instr(char *buff);
 void	apply_checker_instr(t_list *instr_list, t_stacks stacks);
 void	clean_up(t_stacks stacks, t_list **instr_list, char *buff);
+
+#endif
