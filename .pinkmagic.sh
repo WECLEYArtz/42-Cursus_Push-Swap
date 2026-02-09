491ea4b61738bf02d286b42dd9b55304e4c28a90
Ahmed Mounsif <ahmounsi@e1r8p11.1337.ma>
2026-02-09 20:25:20 +0100

    test scripts

--- .pinkmagic.sh
+++ .pinkmagic.sh
@@ -3,7 +3,7 @@ CMD="./push_swap \$1 | wc-l"
 RES=$(cat << EOF | python3 | sed -e 's/(/"/g' -e 's/)/"/g' -e 's/,/ /g' | xargs -I ... echo ...
 from itertools import permutations
 
-perm = permutations([1, 2, 3, 4, 5])
+perm = permutations([1, 2, 3])
 
 for i in perm:
     print(i)
@@ -13,7 +13,7 @@ EOF
 while read line; do
 	RES=$(./push_swap $line | wc -l)
 	STATUS=$(./push_swap $line | ./checker_linux $line)
-	if [ $RES -gt 12 ]; then
+	if [ $RES -gt 5 ]; then
 		printf "Failed With $RES Moves For: $line - $STATUS\n" 
 		exit
 	else
