806ffc3228acc2a9a5db831d0f04f4455e6e67d8
Ahmed Mounsif <ahmounsi@e1r8p15.1337.ma>
2026-02-12 11:35:09 +0100

    updated push script

--- .vogsphere_cleanup.sh
+++ .vogsphere_cleanup.sh
@@ -4,21 +4,25 @@ git checkout -b vogsphere
 make fclean
 make -C ./libft fclean
 
-rm -rf *test*
-rm -rf *Test*
-rm -rf a.out*
-
-rm -rf *.txt
-rm -rf .*.txt
-rm -rf *.txt
-
-rm -rf .blackmagic.sh
-rm -rf .gdb.gdb
-rm -rf .gitignore
-rm -rf .gitmodules
-rm -rf .todo.txt
-rm -rf .vogsphere_cleanup.sh
-rm -rf .whitemagic.sh
+rm -rf \
+	*test*\
+	*Test*\
+	a.out*\
+\
+	*.txt\
+	.*.txt\
+	*.txt\
+\
+	.blackmagic.sh\
+	.whitemagic.sh\
+	.pinkmagic.sh\
+\
+	.gdb.gdb\
+	.gitignore\
+	.gitmodules\
+	.todo.txt\
+\
+	.vogsphere_cleanup.sh
 
 git add .
 git commit -m "auto delete"
